feec82a4224f0c705dff889ebcfa3c568f1dc6bf
matthewbillienyc <matthewbillienyc@gmail.com>
2017-01-01 10:33:17 -0500

    react jest testing operational

---
+++ lukather-discography/tests/Year.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Year from '../modules/Year'
+import renderer from 'react-test-renderer'
+
+test('Year creates list items for each album', () => {
+  let props = {
+    year: '1977',
+    albums: [
+      {
+        title: 'Rock On',
+        artist: 'MJ'
+      },
+      {
+        title: 'Album 2',
+        artist: 'Artist 2'
+      }
+    ]
+  }
+  
+  let component = renderer.create(
+    <Year currentYear={props}/>
+  )
+
+  let tree = component.toJSON()
+  
+  expect(tree).toMatchSnapshot()
+})
